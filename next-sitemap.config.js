17ec6077b9cfdac7d3a7e1f7d3eeb31d7e55e10a
kjpmj <kjpmj@tnctec.co.kr>
2022-11-10 12:55:18 +0900

    first

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://vercel-blog-kjpmj.vercel.app',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,19 +1,21 @@
 import Head from 'next/head'
 import Date from '../../components/Date'
 import Layout from '../../components/Layout'
-import { getPostData } from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts'
 import utilStyles from '../../styles/utils.module.css'
 import { useRouter } from 'next/router'
+import { MDXRemote } from 'next-mdx-remote'
+import CodeBlock from '../../components/CodeBlock'
 
 export async function getStaticPaths() {
-  // const paths = getAllPostIds()
-  const paths = [
-    {
-      params: {
-        id: 'ssg-ssr',
-      },
-    },
-  ]
+  const paths = getAllPostIds()
+  // const paths = [
+  //   {
+  //     params: {
+  //       id: 'ssg-ssr',
+  //     },
+  //   },
+  // ]
 
   // const paths = [
   //   // { params: { id: 'pre-rendering' } },
@@ -27,8 +29,6 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params, ...props }) {
-  console.log(params)
-  console.log(props)
   const postData = await getPostData(params.id)
   return {
     props: {
@@ -36,6 +36,19 @@ export async function getStaticProps({ params, ...props }) {
     },
   }
 }
+
+const Button = ({ children }) => {
+  return (
+    <button
+      className="bg-black dark:bg-white text-lg dark:text-teal-700 text-teal-200 rounded-lg px-5"
+      onClick={() => alert(`으으아`)}
+    >
+      {children}
+    </button>
+  )
+}
+
+const components = { Button, CodeBlock }
 
 export default function Post({ postData }) {
   const router = useRouter()
@@ -54,7 +67,12 @@ export default function Post({ postData }) {
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        {postData.contentHtml && (
+          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        )}
+        {postData.mdxSource && (
+          <MDXRemote {...postData.mdxSource} components={components} />
+        )}
       </article>
     </Layout>
   )
